710a828dac69d2cd0d0aee69ca76024a3a48bb34
hyun3138 <namhyun3418@gmail.com>
2025-08-22 11:41:16 +0900

    com

--- src/main/java/com/example/med/service/UserService.java
+++ src/main/java/com/example/med/service/UserService.java
@@ -3,7 +3,7 @@ package com.example.med.service;
 import com.example.med.dto.UserInfo;
 import com.example.med.dto.UserSignupRequestDto;
 import com.example.med.dto.UserLoginRequestDto;
-import com.example.med.mapper.UserInfoMapper;
+import com.example.med.mapper.second.UserInfoMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
