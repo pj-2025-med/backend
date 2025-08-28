d99774cf5e5e2ac543dfac7ab0be7c79137b0750
fbwoduf112 <fbwoduf2@gmail.com>
2025-08-28 12:42:10 +0900

    주석 입력 ã로그 완료

--- src/main/java/com/example/med/service/AnnotationService.java
+++ src/main/java/com/example/med/service/AnnotationService.java
@@ -1,9 +1,12 @@
 package com.example.med.service;
 
 import com.example.med.dto.AnnotationDto;
+import com.example.med.dto.logDto.AnnoDto;
+import com.example.med.mapper.StudyCommentMapper;
 import com.example.med.mapper.second.AnnotationMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,11 +15,13 @@ import java.util.List;
 public class AnnotationService {
 
     private final AnnotationMapper annotationMapper;
+    private final StudyCommentMapper studyCommentMapper;
 
     public List<AnnotationDto> getAnnotations(long studyKey, long seriesKey, long imageKey, Integer frameNo) {
         return annotationMapper.findAnnotations(studyKey, seriesKey, imageKey, frameNo);
     }
 
+    @Transactional
     public void saveAnnotations(long studyKey, long seriesKey, long imageKey, Integer frameNo, String annotations, String userId) {
         AnnotationDto dto = new AnnotationDto();
         dto.setStudyKey(studyKey);
@@ -26,7 +31,12 @@ public class AnnotationService {
         dto.setAnnotations(annotations);
         dto.setCreatedBy(userId);
 
+        AnnoDto annoDto = AnnotationDto.toAnnoDto(dto);
+        annoDto.setCreatedBy(userId);
         // H2 DB는 MERGE, Oracle은 MERGE INTO를 사용해 UPSERT 처리
         annotationMapper.upsertAnnotations(dto);
+        studyCommentMapper.insertAnnoLog(annoDto);
     }
+
+
 }
